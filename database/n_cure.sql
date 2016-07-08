d196ec1410a034fda66882b97d38e31c44066e5f
Yehan Pemarathne <yehancha@gmail.com>
2016-07-08 18:23:24 +0530

    Update database script

--- database/n_cure.sql
+++ database/n_cure.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 28, 2016 at 01:01 PM
+-- Generation Time: Jul 08, 2016 at 06:20 PM
 -- Server version: 5.6.16-log
 -- PHP Version: 5.5.11
 
@@ -28,12 +28,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `appointment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(4) NOT NULL,
+  `user_id` varchar(4) NOT NULL,
   `time` datetime NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `user_id`, `time`, `description`) VALUES
+(1, '1111', '2016-05-28 00:10:00', 'Test3'),
+(2, '1111', '2016-05-28 00:10:00', 'Test3'),
+(3, '1111', '2016-05-28 00:10:00', 'Test4'),
+(4, '1111', '2016-05-28 00:10:00', 'Test4'),
+(5, '1111', '2016-05-28 00:10:00', 'Test5');
 
 -- --------------------------------------------------------
 
@@ -48,9 +59,27 @@ CREATE TABLE IF NOT EXISTS `patient` (
   `city` varchar(64) NOT NULL,
   `description` text,
   `disease` varchar(255) NOT NULL,
-  `version` int(11) NOT NULL DEFAULT '1',
+  `last_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`id`, `name`, `address`, `city`, `description`, `disease`, `last_updated`) VALUES
+(1, 'Navin', 'Chathura', 'Divulapitiya', 'No illness', 'Nothing', '2016-05-29 09:14:05'),
+(2, 'Yehan', 'Chathura', 'Divulapitiya', 'No illness', 'Nothing', '2016-05-29 09:12:43'),
+(3, 'Yehan', 'Chathura', 'Divulapitiya', 'No illness', 'Nothing', '2016-05-29 09:13:33'),
+(4, 'Yehan', 'Chathura', 'Divulapitiya', 'No illness', 'Nothing', '2016-05-29 09:14:05'),
+(5, 'Yehan', 'Chathura', 'Divulapitiya', 'No illness', 'Nothing', '2016-05-29 09:14:22'),
+(6, 'Yehan', 'Chathura', '', 'No illness', 'Nothing', '2016-05-29 09:25:36'),
+(7, 'Yehan', 'Chathura', 'Divulapitiya', 'No illness', 'Nothing', '2016-05-29 09:18:37'),
+(8, 'Yehan', 'Chathura', 'Divulapitiya', 'No illness', 'Nothing', '2016-05-29 09:20:08'),
+(9, 'Yehan', 'Chathura', 'Divulapitiya', 'No illness', 'Nothing', '2016-05-29 09:23:08'),
+(10, 'Navin', 'Chathura', 'div', 'illness', 'Nothing', '2016-05-29 09:25:24'),
+(11, 'Navin', 'Chathura', 'div', 'illness', 'Nothing', '2016-05-29 09:25:36'),
+(12, 'Navin', 'Chathura', 'div', 'illness', 'Nothing', '2016-05-29 09:26:43');
 
 -- --------------------------------------------------------
 
@@ -72,11 +101,18 @@ CREATE TABLE IF NOT EXISTS `photo` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `id` varchar(4) NOT NULL,
   `name` varchar(255) NOT NULL,
   `last_logged_in` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `last_logged_in`) VALUES
+('1111', 'Yehan', NULL);
 
 --
 -- Constraints for dumped tables
